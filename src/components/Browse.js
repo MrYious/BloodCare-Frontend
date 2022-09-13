cc0aecd5c4d53b896bdc3c07ff82f482a0a525ba
MrYious <rosariomark37@gmail.com>
2022-09-13 20:07:00 +0800

    feat: added cards

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -18,15 +18,23 @@ const Browse = () => {
 
     return (<>
         <div className="flex items-center justify-center w-full h-full gap-5 px-4 py-6 overflow-y-hidden ">
-            <div className="flex flex-col w-full h-full border-2 border-red-900 shadow-sm rounded-3xl shadow-red-900">
+            <div className="flex flex-col w-full h-full border-2 border-red-900 shadow-sm rounded-t-3xl shadow-red-900">
                 <div className="px-8 py-3 text-xl font-semibold">
-                    LIST OF DONORS
+                    LIST OF NEAREST DONORS (Under Development)
                 </div>
-                <div className="flex flex-wrap items-center justify-center gap-3 px-6 py-4 overflow-y-auto bg-slate-200">
+                <div className="flex flex-wrap items-center justify-center gap-4 px-3 py-2 overflow-y-auto">
                     {
-                        users.map((user, i) =>  {return <div key={i} className="flex flex-col w-56 h-40 p-3 text-sm border-2 border-black rounded-lg shadow-sm cursor-pointer shadow-black">
-                            <div>{user.name}</div>
-                        </div>})
+                        users.map((user, i) =>  {
+                            return <div key={i} className="flex flex-col h-48 p-3 text-sm border-2 border-black rounded-lg shadow-sm cursor-pointer w-60 shadow-black">
+                                <div>{user.name}</div>
+                                <div>20 yrs old</div>
+                                <div>Male</div>
+                                <div>O+</div>
+                                <div>Brgy: Tondo</div>
+                                <div>City: Manila</div>
+                                <div>Province: NCR</div>
+                                <div>⭐⭐⭐⭐⭐⭐</div>
+                            </div>})
                     }
                 </div>
             </div>
